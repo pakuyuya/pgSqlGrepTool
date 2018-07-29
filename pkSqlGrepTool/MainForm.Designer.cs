65a29f14a07fea02cfa707005842833871fd0c4f
pakuyuya <pakuyuya@yahoo.co.jp>
2018-07-30 00:18:25 +0900

    change font

--- pkSqlGrepTool/MainForm.Designer.cs
+++ pkSqlGrepTool/MainForm.Designer.cs
@@ -177,6 +177,7 @@
             // txContent
             // 
             this.txContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txContent.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txContent.Location = new System.Drawing.Point(0, 0);
             this.txContent.Multiline = true;
             this.txContent.Name = "txContent";
